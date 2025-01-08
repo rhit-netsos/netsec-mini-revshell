a95c1600617a2cf40da756778b33b420476bc1b0
Mohammad Noureddine <noureddi@rose-hulman.edu>
2025-01-08 16:06:58 -0500

    ready to be distributed

---
+++ volumes/src/simple_prompt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  char input[1025];
+
+  printf("[%s] Running process with pid = %d...\n", argv[0], getpid());
+
+  printf("[%s] Enter a message here: ", argv[0]);
+  fflush(stdout);
+  fgets(input, 1024, stdin);
+  printf("[%s] Received %s\n", argv[0], input);
+  while(1);
+
+  exit(0);
+}
